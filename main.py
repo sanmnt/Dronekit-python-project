edd6f14d55b130658f1db9a828fcf628ceead12d
sanmnt <30962501+sanmnt@users.noreply.github.com>
2021-04-14 17:36:19 -0700

    test

--- main.py
+++ main.py
@@ -1,58 +1,135 @@
-
-print "Start simulator (SITL)"
+from __future__ import print_function
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative
-import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
 
-# Import DroneKit-Python
 
-# Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
+# Set up option parsing to get connection string
+import argparse
+
+from pymavlink import mavutil
+
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
 
-# Get some vehicle attributes (state)
-print "Get some vehicle attribute values:"
-print " GPS: %s" % vehicle.gps_0
-print " Battery: %s" % vehicle.battery
-print " Last Heartbeat: %s" % vehicle.last_heartbeat
-print " Is Armable?: %s" % vehicle.is_armable
-print " System status: %s" % vehicle.system_status.state
-print " Mode: %s" % vehicle.mode.name    # settable
-
 def arm_and_takeoff(aTargetAltitude):
-#take off to target altitude
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
     print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
-        print("waiting to initialize")
+        print(" Waiting for vehicle to initialise...")
         time.sleep(1)
 
     print("Arming motors")
+    # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
-    while not vehicle.mode.name == 'GUIDED':
-        print("wait")
-        time.sleep(1)
 
+    # Confirm vehicle armed before attempting to take off
     while not vehicle.armed:
-        print("Waiting for vehicle to arm....")
+        print(" Waiting for arming...")
         time.sleep(1)
 
-    print("Taking off")
-    vehicle.simple_takeoff(aTargetAltitude)
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
     while True:
-        print("Altitude: ", vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * .95:
-            print("Altitude reached")
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
             break
         time.sleep(1)
+
+
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0,duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
+def condition_yaw(heading, relative=False):
+    if relative:
+        is_relative=1 #yaw relative to direction of travel
+    else:
+        is_relative=0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+
 arm_and_takeoff(10)
+
+print("Set default/target airspeed to 3")
+vehicle.airspeed = 3
+
+#NOTE: PUT FOR LOOP AND CONNECT IT TO LIDAR SO THAT WE HAVE A FOR LOOP THAT IF FRONT AREA IS CLEAR MOVE FORWARD
+send_ned_velocity(1, 0, 0, 20)
+# print("Going towards first point for 30 seconds ...")
+# point1 = LocationGlobalRelative(-35.361354, 149.165218, 20)
+# vehicle.simple_goto(point1)
+
+# sleep so we can see the change in map
+time.sleep(30)
+
+# print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+# point2 = LocationGlobalRelative(-35.363244, 149.168801, 20)
+# vehicle.simple_goto(point2, groundspeed=10)
+
+# sleep so we can see the change in map
+time.sleep(30)
+
+print("Returning to Launch")
+vehicle.mode = VehicleMode("RTL")
+
 # Close vehicle object before exiting script
+print("Close vehicle object")
 vehicle.close()
 
-
-# Shut down simulator
-sitl.stop()
-print("Completed")
+# Shut down simulator if it was started.
+if sitl:
+    sitl.stop()
